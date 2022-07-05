879f9b13485c0392d21b7ae3f92cfee624163967
Akande Al-ameen <longlasting1805@gmail.com>
2022-07-05 15:57:11 +0100

    project name and application name refactored

--- blogAppication/wsgi.py
+++ blogAppication/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for BlogAppication project.
+WSGI config for blogAppication project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlogAppication.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogAppication.settings')
 
 application = get_wsgi_application()
